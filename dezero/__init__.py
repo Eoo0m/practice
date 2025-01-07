985ea3ccfdccbdfe3f4d668b2bd5a178c1e22e99
Eoo0m <jseom818@gmail.com>
2025-01-07 07:00:02 +0000

    Add core_simple.py inside dezero folder

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -7,7 +7,9 @@ from dezero.core import as_array
 from dezero.core import as_variable
 from dezero.core import setup_variable
 from dezero.core import Parameter
+from dezero.layers import Layer
 import dezero.utils
 import dezero.functions
+
 
 setup_variable()
